a483f3a08d35c980ae6d7b26b60ee07862091bff
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-07-21 08:44:30 -0300

    ...

--- main.py
+++ main.py
@@ -12,7 +12,12 @@ class OpenCvTests:
   
 
   
-  def showContours(self,img : np.ndarray): 
+  def showContours(self): 
+    img = np.zeros((200,200),dtype=np.uint8) 
+    squareWidth = 100
+    x,y = 50,50
+    img[x:x+squareWidth,y:y+squareWidth] = 255
+    img[10:25,10:25] = 255 
     _ , thresh = cv2.threshold(img,127,255,0)
     contours , hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     contourColor = (0,255,0)
@@ -51,41 +56,47 @@ class OpenCvTests:
      cv2.waitKey()
      cv2.destroyAllWindows()
      
+  def roundingCircles(self, file_name : str): 
+     planets = cv2.imread(file_name)
+     grayedImg = cv2.cvtColor(planets, cv2.COLOR_BGR2GRAY)
+     grayedImg = cv2.medianBlur(grayedImg,5)
+     circles = cv2.HoughCircles(grayedImg,cv2.HOUGH_GRADIENT, 1,120,param1=100,param2=30,minRadius=0,maxRadius=0)
+     circles = np.uint16(np.around(circles))
+     for elem in circles[0,:]: 
+        cv2.circle(planets,(elem[0],elem[1]),elem[2],(0,255,0),2)
+        cv2.circle(planets,(elem[0],elem[1]),2,(0,0,255),3)
+     cv2.imshow('Hough circles',planets)
+     cv2.waitKey()
+     cv2.destroyAllWindows()     
+     
   def showContours2(self,file_name : str): #desennhado os contornos manualmente
     img = cv2.pyrDown(cv2.imread(file_name,cv2.IMREAD_UNCHANGED))
     
-    #grayedImg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret , thresh = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    #img = cv2.Canny(img,10,20)
+    ret , thresholded = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
     
-    contours , hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    colored = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-    #cv2.drawContours(colored,contours,1,(0,255,0),3)
+    contours , hierarchy = cv2.findContours(thresholded,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(img,contours,-1,(0,255,0),1)
     
-    #for contour in contours: 
-    contour = contours[1]
-    #print('contour at idx 1 ',contour)
-    x,y,w,h = cv2.boundingRect(contour)
-    #print('convert to a rect', x,y,w,h)     
-    cv2.rectangle(colored,(x,y),(x+w,y+h),(0,255,0),2)
-    (x,y),radius = cv2.minEnclosingCircle(contour)
+     
+    # contour = contours[1]   
+    # x,y,w,h = cv2.boundingRect(contour)    
+    # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+    # (x,y),radius = cv2.minEnclosingCircle(contour)
     
-    center = (int(x),int(y))
-    radius = int(radius)
-    cv2.circle(colored,center,radius,(0,0,255),2)
+    # center = (int(x),int(y))
+    # radius = int(radius)
+    # cv2.circle(img,center,radius,(0,0,255),2)
     
-    cv2.imshow('',colored)
+    cv2.imshow('',img)
     cv2.waitKey()
     cv2.destroyAllWindows()
        
 def main():
    openCv = OpenCvTests()
-   # img = np.zeros((200,200),dtype=np.uint8) 
-   # squareWidth = 100
-   # x,y = 50,50
-   # img[x:x+squareWidth,y:y+squareWidth] = 255
-   # img[10:25,10:25] = 255 
-   # openCv.showContours(img)
-   openCv.showContours2('./yy.jpg')
+   openCv.roundingCircles('./planets.jpg')
 
 if __name__ == '__main__':
     main()
