962e3885cc9702660a52a4e9e55297daf2bedb04
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-08-14 00:44:44 -0300

    ...

--- object_detector_bow_svm.py
+++ object_detector_bow_svm.py
@@ -9,11 +9,14 @@ class Traineer:
       self.FLANN_INDEX_KDTREE = 1
       self.flann = cv2.FlannBasedMatcher(
         dict(algorithm=self.FLANN_INDEX_KDTREE,trees=5),{})
-      num_clusters = 40
+      num_clusters = 131
       self.bow_kmeans_trainer = cv2.BOWKMeansTrainer( num_clusters )  
       self.bow_extractor = cv2.BOWImgDescriptorExtractor(self.sift,self.flann)
       self.files = []
-      self.dirToWalk = r'.\datasets\5857_1166105_bundle_archive\fruits-360\Test'   
+      self.dirToWalk = r'.\datasets\5857_1166105_bundle_archive\fruits-360\Test'
+      self.training_data = []
+      self.training_labels = []
+      self.svm = None
    def files_contains_class_name_index_of(self, name : str):
        i = 0
        for f in self.files:
@@ -22,74 +25,72 @@ class Traineer:
                return i
            i += 1    
        return -1        
-   def load_files(self):     
-      
+   def load_files(self):          
       i = 0
       for root,dirs,files in os.walk(self.dirToWalk):
           for name in files:
             basename = os.path.basename(root)
             y = self.files_contains_class_name_index_of(basename)
             if (y == -1):
-              y = len(self.files) 
-              i += 1   
-              self.files.append({"index":i,"dirs_per_class":[],"class_name":basename})
+              y = len(self.files)   
+              self.files.append({"index":i,"imgs_per_class":[],"class_name":basename})
+              i += 1 
             row = self.files[y]
-            files_per_class = row["dirs_per_class"]
-            files_per_class.append(name)
+            files_per_class = row["imgs_per_class"]
+            files_per_class.append(os.path.join(root,name))
 
    def fill_bow(self):
-       print( len(self.files) )             
+       for row in self.files:
+          for file_name in row['imgs_per_class'][0:15]: 
+             img = cv2.imread(file_name,cv2.IMREAD_GRAYSCALE)
+             keypoints, descriptors = self.sift.detectAndCompute(img, None)
+             if descriptors is not None:
+                self.bow_kmeans_trainer.add(descriptors)
+       voc = self.bow_kmeans_trainer.cluster()
+       self.bow_extractor.setVocabulary(voc)          
+   def extract_bow_descriptors(self,img : np.ndarray):
+       features = self.sift.detect(img)
+       return self.bow_extractor.compute(img, features)
+   def separate_training_data(self):
+     for row in self.files:
+          for file_name in row['imgs_per_class'][0:15]:
+             img = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
+             descriptors = self.extract_bow_descriptors(img)
+             if descriptors is not None:
+                self.training_data.extend(descriptors)
+                self.training_labels.append(row['index'])
+   def train(self):
+      self.svm = cv2.ml.SVM_create()
+      '''if(os.path.exists('./fruits_svm.xml')):
+         self.svm.load('./fruits_svm.xml')
+         print('loaded SVM from file')
+      else:  ''' 
+      self.svm.train(np.array(self.training_data), cv2.ml.ROW_SAMPLE,
+            np.array(self.training_labels))
+      #self.svm.save('./fruits_svm.xml')
+      #print('saved SVM to file')
+   def run(self):
+      self.load_files()
+      self.fill_bow()
+      self.separate_training_data()
+      self.train()
+   def get_label_name(self,index : int):
+        return self.files[index]['class_name']
+      
+   def test(self):
+      img = cv2.imread('./carambola.jpg')
+      gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      descriptors = self.extract_bow_descriptors(gray_img)
+      prediction = self.svm.predict(descriptors)
+      label_idx = int(prediction[1][0][0])
+      print( self.get_label_name(label_idx) )
 if __name__ == '__main__':
    trainner = Traineer()    
-   trainner.load_files() 
-   trainner.fill_bow()   
-     
-'''
+   trainner.run()
+   trainner.test()  
+''' 
 
-def add_sample(path):
- img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
- keypoints, descriptors = sift.detectAndCompute(img, None)
- if descriptors is not None:
-    bow_kmeans_trainer.add(descriptors)
-
-
-for i in range(BOW_NUM_TRAINING_SAMPLES_PER_CLASS):
- pos_path, neg_path = get_pos_and_neg_paths(i)
- add_sample(pos_path)
- add_sample(neg_path)
- 
-voc = bow_kmeans_trainer.cluster()
-bow_extractor.setVocabulary(voc) 
-
-
-def get_pos_and_neg_paths(i):
- pos_path = 'CarData/TrainImages/pos-%d.pgm' % (i+1)
- neg_path = 'CarData/TrainImages/neg-%d.pgm' % (i+1)
- return pos_path, neg_path
- 
-def extract_bow_descriptors(img):
- features = sift.detect(img)
- return bow_extractor.compute(img, features)
- 
-
-training_data = []
-training_labels = []
-for i in range(SVM_NUM_TRAINING_SAMPLES_PER_CLASS):
- pos_path, neg_path = get_pos_and_neg_paths(i)
- pos_img = cv2.imread(pos_path, cv2.IMREAD_GRAYSCALE)
- pos_descriptors = extract_bow_descriptors(pos_img)
- if pos_descriptors is not None:
-     training_data.extend(pos_descriptors)
-     training_labels.append(1)
-     neg_img = cv2.imread(neg_path, cv2.IMREAD_GRAYSCALE)
-     neg_descriptors = extract_bow_descriptors(neg_img)
- if neg_descriptors is not None:
-     training_data.extend(neg_descriptors)
-     training_labels.append(-1)
-
-svm = cv2.ml.SVM_create()
-svm.train(np.array(training_data), cv2.ml.ROW_SAMPLE,
-            np.array(training_labels))     
+    
      
 for test_img_path in ['CarData/TestImages/test-0.pgm',
      'CarData/TestImages/test-1.pgm',
