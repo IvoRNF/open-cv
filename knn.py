e841077ff3322bccc9d292fdefe7a53e7887d30d
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-10-01 00:30:26 -0300

    ...

--- knn.py
+++ knn.py
@@ -21,7 +21,6 @@ class Knn:
             self.knn = cv2.ml.KNearest_load(self.knn_fname)
             print('loaded') 
         else:
-            print('saving knn to file')  
             self.knn = cv2.ml.KNearest_create()
     def run(self):
         if not self.loaded:
@@ -29,7 +28,6 @@ class Knn:
           self.train_knn()
 
     def train_knn(self):
-
          responses = []
          descriptors = []
          for row in self.files: 
@@ -83,6 +81,7 @@ class Knn:
     def train(self,data , labels):
         self.knn.train(data,cv2.ml.ROW_SAMPLE,labels)
         self.knn.save(self.knn_fname)
+        print('saving knn to file')  
     def processAndPredict(self,sample , k = 20,pyrDownLevels=0):
         sampleToPredict = sample
         if type(sampleToPredict) != np.ndarray:
@@ -99,38 +98,43 @@ class Knn:
         for i in range(levels):
             img = cv2.pyrDown(img)
         return img
+def middleRects(shape,start=2,end=8,step=1):
+   h,w = shape[:2]
+   for i in np.arange(start,end,step):
+      factor = i/10
+      rect_width = int(h * factor) #inverte em paisagem
+      rect_height = int(w * factor)
+      middle_w = w//2
+      middle_h = h//2
+      x = (middle_w - rect_width//2)
+      y = (middle_h - rect_height//2)     
+      yield (x,y,rect_width,rect_height)
+  
 def real_time_test():
    min_distance = 20000.00
    knn = Knn()
    knn.run()
    capture = cv2.VideoCapture(0)
    sucess,frame = capture.read()
-   roi_width = int(frame.shape[1] * 0.5)
-   roi_height = int(frame.shape[0] * 0.5)
-   middle_w = int(frame.shape[1]/2)
-   middle_h = int(frame.shape[0]/2)
-   x = middle_w - int(roi_width / 2)
-   y = middle_h - int(roi_height / 2)
    while (sucess):
       frame_cpy = frame.copy() 
-      cv2.rectangle(frame_cpy,(x,y),(x+roi_height,y+roi_width),(0,255,0),1) #inverte em paisagem
+      cv2.circle(frame_cpy,(frame.shape[1]//2,frame.shape[0]//2),10,(0,255,255),-1)
       cv2.imshow('',frame_cpy)
-      frame_cpy = None
-      k = cv2.waitKey(30)
-      if k == ord('f'):
+      k = cv2.waitKey(5)
+      if k == ord('q'):
           break
-      gap = 10 
-      roi : np.ndarray = frame[y:y+roi_width-gap,x:x+roi_height-gap] 
-      response = knn.processAndPredict(roi)
-      distance = np.sum ( np.squeeze(response[3]) )
-      class_idx = int(response[0])
       sucess,frame = capture.read()
-      if(distance < min_distance):
-          class_name = knn.class_names[class_idx]
-          txt = '%s(%.2f)' % (class_name,distance)
-          cv2.putText(frame,txt,(x,y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),1,cv2.LINE_AA)
-          #print(class_name,end='\n\n')
-       
+      for (x,y,w,h) in middleRects(frame.shape):
+          roi = frame[y:y+h,x:x+w] 
+          response = knn.processAndPredict(roi)
+          distance = np.sum ( np.squeeze(response[3]) )
+          class_idx = int(response[0])
+          if(distance < min_distance):
+            class_name = knn.class_names[class_idx]
+            txt = '%s(%.2f)' % (class_name,distance)
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+            cv2.putText(frame,txt,(x,y),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2) 
+            break 
    capture.release()   
    cv2.destroyAllWindows()    
  
@@ -157,37 +161,42 @@ def evaluate_knn():
        row = knn.files_test[i]
        count_per_class = len(row['imgs_per_class'])
        class_name = row['class_name']
-       print('acurracy %d%s para %s' % ( ((count_corrects//count_per_class) * 100),'%',class_name ))     
+       print('acurracy %.2f%s para %s' % ( ((count_corrects/count_per_class) * 100),'%',class_name ))     
 
 def capture():
    capture = cv2.VideoCapture(0)
    sucess,frame = capture.read()
-   roi_width = int(frame.shape[1] * 0.5)
-   roi_height = int(frame.shape[0] * 0.5)
+   roi_width = int(frame.shape[0] * 0.5) #paisagem
+   roi_height = int(frame.shape[1] * 0.5)
    middle_w = int(frame.shape[1]/2)
    middle_h = int(frame.shape[0]/2)
-   x = middle_w - int(roi_width / 2)
+   x = middle_w  - int(roi_width / 2)
    y = middle_h - int(roi_height / 2)
-   i = 1000
+   i = 1
    while (sucess):
      frame_cpy = frame.copy() 
-     cv2.rectangle(frame_cpy,(x,y),(x+roi_height,y+roi_width),(0,255,0),1) #inverte em paisagem
+     cv2.rectangle(frame_cpy,(x,y),(x+roi_width,y+roi_height),(0,255,0),1) #inverte em paisagem
      cv2.imshow('',frame_cpy)
      frame_cpy = None
      k = cv2.waitKey(30)
-     if k == ord('f'):
+     if k == ord('q'):
         break
      gap = 10 
      if k == ord('l'): #salva foto de leite lata
         i = i + 1
-        roi = frame[y:y+roi_width-gap,x:x+roi_height-gap]
+        roi = frame[y:y+roi_height-gap,x:x+roi_width-gap]
         roi = cv2.resize(roi,(150,200),interpolation=cv2.INTER_AREA)
         cv2.imwrite('./datasets/captures/leite_lata/%d.jpg'%(i),roi)
      elif k == ord('c'): #salva foto de leite caixa
         i = i + 1
-        roi = frame[y:y+roi_width-gap,x:x+roi_height-gap]
+        roi = frame[y:y+roi_height-gap,x:x+roi_width-gap]
         roi = cv2.resize(roi,(150,200),interpolation=cv2.INTER_AREA)
         cv2.imwrite('./datasets/captures/leite_caixa/%d.jpg'%(i),roi)   
+     elif k == ord('f'): 
+        i = i + 1
+        roi = frame[y:y+roi_height-gap,x:x+roi_width-gap]
+        roi = cv2.resize(roi,(150,200),interpolation=cv2.INTER_AREA)
+        cv2.imwrite('./datasets/captures/fermento/%d.jpg'%(i),roi)     
      sucess,frame = capture.read()     
    capture.release()   
    cv2.destroyAllWindows()
@@ -208,15 +217,6 @@ def pyramid(img, scale_factor=1.25, min_size=(150,200),max_size=(600, 600)):
       w /= scale_factor
       h /= scale_factor
       img = cv2.resize(img, (int(w), int(h)),interpolation=cv2.INTER_AREA)  
-def pyramidByIndex(frame ,scale_factor=1.25, min_size=(100,50),max_size=(600,600), pos=3):
-  result = frame  
-  i = 0 
-  for img in pyramid(frame,scale_factor,min_size,max_size):
-     if i>= pos:
-       break
-     result = img 
-     i += 1
-  return result
 def scale_rect(shape_origin, shape_dest,rect):
      x,y,w,h = rect 
      scaleH = shape_dest[0]/float(shape_origin[0])
@@ -239,8 +239,9 @@ def detect_mult_scale(img,threashold=20000.00,winSize=(70,100),winStep=20,knn=No
               result = (predicted_class_idx,distance,scale_rect(resized.shape,img.shape,(x,y,w,h)))
               min_distance = distance
            if distance <= threashold:
-             return result   
+              return result   
   return result         
+
 
 def main():
     print('1 para evaluate \n2 para real time test\n3 capturar ')
@@ -250,29 +251,7 @@ def main():
     elif v=='2':
       real_time_test()
     elif v=='3':
-      capture()  
-    else:
-      f1 = r'C:\Users\Ivo Ribeiro\Documents\open-cv\datasets\originals\leite_po\IMG_20200910_125946964_BURST003.jpg'
-      f2 = r'C:\Users\Ivo Ribeiro\Documents\open-cv\datasets\meus_produtos\creme_leite_\IMG_20200829_094657.jpg'
-      f3 = r'C:\Users\Ivo Ribeiro\Documents\open-cv\datasets\originals\creme_leite\IMG_20200829_094657.jpg'
-      img = cv2.imread(f3)
-      img = cv2.resize(img,(312,416))
-      print(img.shape)  
-      frame = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-      knn = Knn()
-      knn.run()
-      start_time = time.time()
-      class_idx,distance,(x,y,w,h) = detect_mult_scale(frame,knn=knn)    
-      print("%.2f seconds in detect_mult_scale " % (time.time() - start_time))
-      img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-      print(class_idx) 
-      print(distance)  
-      while(True):
-        cv2.imshow('',img)
-        k = cv2.waitKey(50)
-        if k == ord('f'):
-          break
-      cv2.destroyAllWindows()
+      capture()
 
 
 if __name__ == '__main__':
