9578eed1b737f2d91b3c6b2a70c84a33d0323520
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-11-17 08:35:45 -0300

    + backpropagation

--- nn.py
+++ nn.py
@@ -4,39 +4,47 @@ inputs = np.array([4,5,7,10])
 
 desired_outputs = np.array([8,10,14,20])
 
-max_output = np.max(desired_outputs)
-
-weights = np.array([0.05,.1]) #bias and weight
+weights = np.array([0.05,1],dtype=np.float32) #bias and weight
 
 
-def prediction_error(desired,expected): 
-    return np.abs(np.mean( desired-expected  ))
+def square_error(desired,predicted): 
+    return  np.power(desired-predicted,2)
 
-def activation_function(input_vl):
-    global max_output
-    if input_vl > max_output: 
-        return max_output 
+def relu(input_vl):
+    if input_vl < 0: 
+        return 0
     else: 
-      return input_vl      
-def update_weights(predicted , idx, learning_rate = 0.00001): 
+        return input_vl     
+def sigmoid(input_vl):
+    return 1/(1+np.power(np.e,-input_vl) )    
+
+def update_weights(predicted , idx, learning_rate = 0.0001): 
     global desired_outputs
     global inputs
     global weights 
-    weights = weights + learning_rate * (desired_outputs[idx] - predicted)* inputs[idx]
+    der_magn = 0    #backpropagation
+    der_magn = (inputs[idx] * (2 * (predicted - desired_outputs[idx])))
+    if der_magn==0:
+        return weights
+    elif der_magn> 0:
+      weights[1] = weights[1] - (learning_rate * np.abs(der_magn))  
+    else:
+      weights[1] = weights[1] + (learning_rate * np.abs(der_magn))  
     return weights
 
 
-def training_loop(max_iterations=9000): 
+def training_loop(max_iterations=2000): 
     global inputs 
     global weights
     global desired_outputs
-    error = 1 
+    abs_error = 1 
     idx = 0 
     i= 0 
-    while((i < max_iterations) or (error>=0.01)): 
+    while (i < max_iterations) or (abs_error>=0.01)  : 
         z = weights[0] * 1 + weights[1] * inputs[idx]
-        predicted = activation_function(z)
-        error = prediction_error(desired_outputs[idx],predicted)
+        predicted = relu(z)
+        error = square_error(desired_outputs[idx],predicted)
+        abs_error = np.abs( error )
         weights = update_weights(predicted,idx)
         idx += 1 
         idx = idx % inputs.shape[0]
@@ -44,14 +52,11 @@ def training_loop(max_iterations=9000):
     return error     
 
 if __name__ == '__main__': 
+    print('ok')
     error = training_loop()
     print('End training.')
     print('Weights ',weights)
-    #new_inputs = np.array([25,60,65,50])
-    #new_desired_outputs = np.array([50,120,130,100])
-    #max_output = np.max(new_desired_outputs)
-    #new_inputs = [4,5,7,10]
-    for input_vl in inputs: 
+    for input_vl in [20,30,40,50,2,5]: 
         z = weights[0] * 1 + weights[1] * input_vl
-        predicted = activation_function(z)
+        predicted = relu(z)
         print('%.2f predicted as %.2f' % (input_vl,predicted))    
