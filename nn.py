96d34272006c98a879f2e5a0a01d33f8ef81525d
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-11-18 08:28:09 -0300

    ...

--- nn.py
+++ nn.py
@@ -1,62 +1,60 @@
 import numpy as np 
 
-inputs = np.array([4,5,7,10])
 
-desired_outputs = np.array([8,10,14,20])
+class MyNeuralNetwork:
 
-weights = np.array([0.05,1],dtype=np.float32) #bias and weight
+    def __init__(self,max_iterations=2000,learning_rate = 0.0001):
+        self.max_iterations= max_iterations
+        self.learning_rate = learning_rate
 
+    def fit(self,x:np.ndarray,y:np.ndarray):
+        self.inputs = x 
+        self.desired_outputs = y
+        self.weights = np.random.uniform(low=-0.1,high=0.1,size=(2)) 
+        self.weights[0]=0.05#bias  
+        self.train() 
 
-def square_error(desired,predicted): 
-    return  np.power(desired-predicted,2)
+    def square_error(self,desired,predicted): 
+         return  np.power(desired-predicted,2)
 
-def relu(input_vl):
-    if input_vl < 0: 
-        return 0
-    else: 
-        return input_vl     
-def sigmoid(input_vl):
-    return 1/(1+np.power(np.e,-input_vl) )    
+    def relu(self,input_vl):
+        if input_vl < 0: 
+            return 0
+        else: 
+            return input_vl     
+    def sigmoid(self,input_vl):
+        return 1/(1+np.power(np.e,-input_vl) )    
 
-def update_weights(predicted , idx, learning_rate = 0.0001): 
-    global desired_outputs
-    global inputs
-    global weights 
-    der_magn = 0    #backpropagation
-    der_magn = (inputs[idx] * (2 * (predicted - desired_outputs[idx])))
-    if der_magn==0:
-        return weights
-    elif der_magn> 0:
-      weights[1] = weights[1] - (learning_rate * np.abs(der_magn))  
-    else:
-      weights[1] = weights[1] + (learning_rate * np.abs(der_magn))  
-    return weights
-
-
-def training_loop(max_iterations=2000): 
-    global inputs 
-    global weights
-    global desired_outputs
-    abs_error = 1 
-    idx = 0 
-    i= 0 
-    while (i < max_iterations) or (abs_error>=0.01)  : 
-        z = weights[0] * 1 + weights[1] * inputs[idx]
-        predicted = relu(z)
-        error = square_error(desired_outputs[idx],predicted)
-        abs_error = np.abs( error )
-        weights = update_weights(predicted,idx)
-        idx += 1 
-        idx = idx % inputs.shape[0]
-        i += 1  
-    return error     
-
+    def update_weights(self,predicted , idx): 
+        der_magn = 0    #backpropagation
+        der_magn = (self.inputs[idx] * (2 * (predicted - self.desired_outputs[idx])))
+        if der_magn==0:
+            return 
+        elif der_magn> 0:
+            self.weights[1] = self.weights[1] - (self.learning_rate * np.abs(der_magn))  
+        else:
+            self.weights[1] = self.weights[1] + (self.learning_rate * np.abs(der_magn))  
+    def train(self): 
+        abs_error = 1 
+        idx = 0 
+        i= 0 
+        while (i < self.max_iterations) or (abs_error>=0.01)  : 
+            predicted = self.predict(self.inputs[idx])
+            error = self.square_error(self.desired_outputs[idx],predicted)
+            abs_error = np.abs( error )
+            self.update_weights(predicted,idx)
+            idx += 1 
+            idx = idx % self.inputs.shape[0]
+            i += 1  
+    def predict(self,input_vl):         
+        z = self.weights[0] * 1 + self.weights[1] * input_vl
+        return self.relu(z)
 if __name__ == '__main__': 
-    print('ok')
-    error = training_loop()
-    print('End training.')
-    print('Weights ',weights)
-    for input_vl in [20,30,40,50,2,5]: 
-        z = weights[0] * 1 + weights[1] * input_vl
-        predicted = relu(z)
-        print('%.2f predicted as %.2f' % (input_vl,predicted))    
+    inputs = np.array([4,5,7,10])
+    outputs = np.array([8,10,14,20])  
+    nn = MyNeuralNetwork()
+    nn.fit(x=inputs,y=outputs)
+    print('Weights ',nn.weights)
+    for input_vl in [20,30,40,50,2,5]:
+        predicted = nn.predict(input_vl)
+        print('%.2f predicted as %.2f' % (input_vl,predicted))
