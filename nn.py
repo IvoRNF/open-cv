3934f4e48fb68230f2605c7c37b7fe99dc6a6d83
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-11-23 07:20:13 -0300

    ...

--- nn.py
+++ nn.py
@@ -14,8 +14,7 @@ class MyNeuralNetwork:
         self.inputs = x 
         self.desired_outputs = y
         if self.hidden_layer_sizes is None: #no hidden layer 
-           self.weights = np.random.uniform(low=-0.1,high=0.1,size=( self.inputs.shape[1]+1 )) 
-           self.weights[0]=0.05#bias  
+           self.weights = np.random.uniform(low=-0.1,high=0.1,size=( self.inputs.shape[1] )) 
         else: 
           last_input_sz =  self.inputs.shape[1]
           self.weights = []
@@ -32,42 +31,46 @@ class MyNeuralNetwork:
         result = input_vl
         result[result < 0] = 0 
         return result 
-    def sigmoid(self,input_vl):
+    def sigmoid(self,input_vl): #nao testado
         return 1.0/(1+np.power(np.e,-1 * input_vl) )    
 
     def update_weights(self,predicted , idx): 
         der_magn = 0    #backpropagation
-        der_magn = (self.inputs[idx][0] * (2 * (predicted - self.desired_outputs[idx][0])))
+        der_magn = (self.inputs[idx][0] * (2 * (predicted[0] - self.desired_outputs[idx][0])))
         if der_magn==0:
             return 
         elif der_magn> 0:
-            self.weights[1] = self.weights[1] - (self.learning_rate * np.abs(der_magn))  
+            self.weights = self.weights - (self.learning_rate * np.abs(der_magn))  
         else:
-            self.weights[1] = self.weights[1] + (self.learning_rate * np.abs(der_magn))  
+            self.weights = self.weights + (self.learning_rate * np.abs(der_magn))  
     def train(self): 
         abs_error = 1 
         idx = 0 
         i= 0 
         while (i < self.max_iterations) or (abs_error>=0.01)  : 
             predicted = self.predict(self.inputs[idx])
-            error = self.square_error(self.desired_outputs[idx][0],predicted)
+            error = self.square_error(self.desired_outputs[idx][0],predicted[0])
             abs_error = np.abs( error )
             self.update_weights(predicted,idx)
             idx += 1 
             idx = idx % self.inputs.shape[0]
             i += 1     
     def predict(self,input_vl):         
-        z = self.weights[0] * 1 + self.weights[1] * input_vl[0]
+        arr = input_vl * self.weights
         func_name = self.activation_func
         func = getattr(self,func_name)
-        return func(np.array([z]))
+        return func(arr)
 if __name__ == '__main__': 
-    inputs = np.array([[3],[4],[7],[10]])
-    outputs = np.array([[9],[12],[21],[30]])  
+    inputs_train = np.array([[3],[4],[7],[10]])
+    outputs_train = np.array([[6],[8],[14],[20]])  
     nn = MyNeuralNetwork()#hidden_layer_sizes=np.array([150,60]),output_layer_size=4)
-    nn.fit(x=inputs,y=outputs)
+    nn.fit(x=inputs_train,y=outputs_train)
     print('Weights ',nn.weights)
-    for input_vl in [[20],[30],[40],[50],[2],[5]]:
-        predicted = nn.predict(input_vl)
-        print('%.2f predicted as %.2f' % (input_vl[0],np.round(predicted)))
+    inputs_test = np.array([[20],[30],[40],[50],[2],[5]])
+    predicted_vls = nn.predict(inputs_test)
+    i = 0 
+    for predicted in predicted_vls:     
+      print('%.2f predicted as %.2f' % (inputs_test[i],np.round(predicted)))
+      i += 1 
+
     
