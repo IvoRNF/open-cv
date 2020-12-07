1f967b762c891b1551442867b77aff2508492327
IvoRibeiro <ivoribeirodonascimento@gmail.com>
2020-12-07 09:22:28 -0300

    ...

--- nn_test.py
+++ nn_test.py
@@ -1,125 +1,136 @@
 import numpy as np 
 from ga import GeneticAlgorithm
 import pickle 
-import os 
-logging = True 
-weights = []
-biases = []
-hidden_layer_sizes = [2]
-output_layer_size = 2 
-model_f_name = './datasets/my_nn77.pk'
-inpt_layer_size = 3
+import os
 
-def log(msg=None):
-    global logging
-    if not logging:
-        return 
-    if msg is None:
-      print()
-    else:       
-      print(msg) 
-def init_bias():
-    global biases
-    biases = np.random.uniform(low=-0.1,high=0.1,size=(len(weights)))      
 
-def init_weights():
-    global hidden_layer_sizes
-    global output_layer_size
-    global weights
-    global inpt_layer_size
-    last_input_sz =  inpt_layer_size
-    for neuron_count in [*hidden_layer_sizes,output_layer_size]: 
-        weights_of_layer = np.random.uniform(low=-0.1,high=0.1,size=(last_input_sz,neuron_count))
-        weights.append(weights_of_layer) 
-        last_input_sz = neuron_count  
-def softmax(inpt):
-    return np.exp(inpt)/np.sum( np.exp(inpt) )    
-def sigmoid(input_vl): 
-    return 1.0/(1.0+np.exp(-1 * input_vl))  
 
-def predict(inpt):
-    global weights
-    global biases 
-    y = inpt
-    for i in range(len(weights)):
-        w = weights[i]
-        b = biases[i]
-        y = np.dot(y,w)  + b
-        y = sigmoid(y)
-    return y 
-def weights_flattened():
-    global weights
-    result = []
-    for layer in weights: 
-        for vl in layer.flat: 
-           result.append(vl)
-    return np.array(result,dtype=np.float64)
-def weights_unflattened(flattened):
-    global weights
-    if len(weights)==0:
-        init_weights()
-    result = [] 
-    start_idx = 0
-    for layer in weights: 
-       wts_of_layer = flattened[start_idx: start_idx + layer.shape[0]*layer.shape[1] ] 
-       start_idx += (len(wts_of_layer)) 
-       wts_of_layer = np.array(wts_of_layer,dtype=np.float64)
-       wts_of_layer = np.reshape(wts_of_layer,newshape=layer.shape) 
-       result.append(wts_of_layer) 
-    return result    
+class MyNeuralNetwork:
 
-def eval_model(ret_stats=False):
-    x_test = [[0,0,255],[0,100,0],[0,0,255],[0,0,255],[0,255,0],[0,255,0],[0,255,0]]
-    y_test = [0,1,0,0,1,1,1]
-    acc = 0
-    c_correct = []
-    probs = []
-    for x,y in zip(x_test,y_test): 
-        arr = predict(x)
-        label = np.argmax(arr)
-        if(label == y):    
-           c_correct.append(1)
-           probs.append(arr)
-           prob = arr[label]
-           if prob > 0:        
-             acc += prob   
-    acc = (acc / len(y_test))
-    if ret_stats:
-        return {'acc_float': acc, 'acc_int':len(c_correct)/len(y_test),'probs':[ (y_test[x],probs[x]) for x in np.arange(len(y_test)) ]}         
-    return acc
-def fitness_func(sol):
-    global weights
-    weights = weights_unflattened(sol)
-    return eval_model()
+    def __init__(self,inpt_layer_size,hidden_layer_sizes,output_layer_size,model_f_name,logging=True):
+        self.model_f_name = model_f_name
+        self.inpt_layer_size = inpt_layer_size 
+        self.hidden_layer_sizes = hidden_layer_sizes 
+        self.output_layer_size = output_layer_size
+        self.logging = logging
+        self.weights = []
+        self.biases = []
+
+
+    def log(self,msg=None):
+        if not self.logging:
+            return 
+        if msg is None:
+           print()
+        else:       
+           print(msg) 
+
+    def init_bias(self):
+        self.biases = np.random.uniform(low=-0.1,high=0.1,size=(len(self.weights)))      
+
+    def init_weights(self):
+        last_input_sz =  self.inpt_layer_size
+        for neuron_count in [*self.hidden_layer_sizes,self.output_layer_size]: 
+            weights_of_layer = np.random.uniform(low=-0.1,high=0.1,size=(last_input_sz,neuron_count))
+            self.weights.append(weights_of_layer) 
+            last_input_sz = neuron_count 
+
+    def softmax(self,inpt):
+        return np.exp(inpt)/np.sum( np.exp(inpt) )  
+
+    def sigmoid(self,input_vl): 
+        return 1.0/(1.0+np.exp(-1 * input_vl))  
+
+    def predict(self,inpt):
+        y = inpt
+        for i in range(len(self.weights)):
+            w = self.weights[i]
+            b = self.biases[i]
+            y = np.dot(y,w)  + b
+            y = self.sigmoid(y)
+        return y 
+    def weights_flattened(self):
+        result = []
+        for layer in self.weights: 
+            for vl in layer.flat: 
+              result.append(vl)
+        return np.array(result,dtype=np.float64)
+    def weights_unflattened(self,flattened):
+        if len(self.weights)==0:
+            self.init_weights()
+        result = [] 
+        start_idx = 0
+        for layer in self.weights: 
+            wts_of_layer = flattened[start_idx: start_idx + layer.shape[0]*layer.shape[1] ] 
+            start_idx += (len(wts_of_layer)) 
+            wts_of_layer = np.array(wts_of_layer,dtype=np.float64)
+            wts_of_layer = np.reshape(wts_of_layer,newshape=layer.shape) 
+            result.append(wts_of_layer) 
+        return result    
+
+    def eval_model(self,ret_stats=False,min_distance=0.3):
+        x_test = [[0,0,255],[0,100,0],[0,0,255],[0,0,255],[0,255,0],[0,255,0],[0,255,0]]
+        y_test = [0,1,0,0,1,1,1]
+        acc = 0
+        c_correct = []
+        probs = []
+        for x,y in zip(x_test,y_test): 
+            arr = self.predict(x)
+            label = np.argmax(arr)
+            probs.append(arr)
+            if(label == y):    
+                c_correct.append(1)
+                prob = arr[label]
+                if prob > 0:                    
+                    has_distance = True
+                    for k in range(len(arr)):
+                        it = arr[k]
+                        if (it > 0) and (k != label):
+                          if (arr[label]  - it)< min_distance:
+                              has_distance = False 
+                              break
+                    if has_distance:          
+                      acc += prob   
+        acc = (acc / len(y_test))
+        if ret_stats:
+            return {'acc_float': acc, 'acc_int':len(c_correct)/len(y_test),'probs':[ (y_test[x],probs[x]) for x in np.arange(len(y_test)) ]}         
+        return acc
+    def fitness_func(self,sol):   
+        self.weights = self.weights_unflattened(sol)
+        return self.eval_model()
 if __name__ == '__main__':
+    model_f_name = './datasets/my_nn77.pk'
+    inpt_sz = 3
+    hidden_szs = [2]
+    outpt_sz = 2
     if not os.path.exists(model_f_name):
-        log('training')
-        logging = False
-        init_weights()
-        init_bias()
-        flattened = weights_flattened()
-        initial_solutions = np.random.uniform(low=-2.0,high=4.0,size=(10,len(flattened)))
+        print('training')
+        nn = MyNeuralNetwork(inpt_sz,hidden_szs,outpt_sz,model_f_name,True)
+        nn.init_weights()
+        nn.init_bias()
+        flattened = nn.weights_flattened()
+        initial_solutions = np.random.uniform(low=-1.0,high=1.0,size=(10,len(flattened)))
         initial_solutions[0] = np.array(flattened,dtype=np.float64)
         ga = GeneticAlgorithm(
             solutions=initial_solutions, 
             num_parents_for_mating=2,
-            generations=100,
-            fitness_func=fitness_func 
+            generations=250,
+            fitness_func=nn.fitness_func 
         )
         ga.start()
-        logging = True
         with open(model_f_name,'wb') as f:
-           mapa = {"weights":ga.solutions[0],"bias":biases} 
+           mapa = {"weights":ga.solutions[0],"bias":nn.biases} 
            pickle.dump(mapa,f)
-        log('saved model to file %s' % (model_f_name))
+        print('saved model to file %s' % (model_f_name))
     else:    
         wts = None
+        nn= MyNeuralNetwork(inpt_sz,hidden_szs,outpt_sz,model_f_name,True)
         with open(model_f_name,'rb') as f:
            mapa = pickle.load(f)
            wts = mapa['weights']
-           biases = mapa['bias']
-        log('loaded model from file')   
-        weights = weights_unflattened(wts)   
-        print(predict([0,0,255]))
-        stats = eval_model(ret_stats=True)
-        log(stats)
+           nn.biases = mapa['bias']
+        print('loaded model from file')   
+        nn.weights = nn.weights_unflattened(wts)   
+        print(nn.predict([0,0,255]))
+        stats = nn.eval_model(ret_stats=True)
+        print(stats)
