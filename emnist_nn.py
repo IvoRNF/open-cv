4c8ecfe6302c17118993ed76e5bc53a5f03f83a8
Ivo Ribeiro <ivoribeirodonascimento@gmail.com>
2022-03-04 14:07:19 -0300

    ...

--- emnist_nn.py
+++ emnist_nn.py
@@ -7,16 +7,43 @@ from torch.utils.data import Subset
 from util import display
 import numpy as np
 from sklearn.model_selection import train_test_split
+from torchvision import transforms
 
 class EmnistNN(nn.Module):
 
     def __init__(self):
+        super().__init__()
         self.root_dir = './emnist'
         self.batch_size = 16
         self.test_sz = 0.15
         self.load_dataset()
-    def forward(self,x):
-        pass
+        self.target_sz = 10
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=20,kernel_size=5,stride=1)
+        self.conv2 = nn.Conv2d(in_channels=20,out_channels=50,kernel_size=5,stride=1)
+
+        self.linear1_in_features = 50 * 4 * 4
+
+        self.fc1 = nn.Linear(in_features=self.linear1_in_features,out_features=500)
+        self.fc2 = nn.Linear(in_features=500,out_features=self.target_sz)
+
+        self.loss_func = nn.MSELoss(reduction='sum') 
+        self.opt = optim.SGD(self.parameters(),lr=1e-4)
+        
+    def forward(self,x_):
+
+        x = torch.unsqueeze(x_,dim=1)
+        x = x.float()
+        x = self.conv1(x)
+        x = nnfunc.relu(x)
+        x = nnfunc.max_pool2d(x,2,2)
+        x = self.conv2(x)
+        x = nnfunc.relu(x)
+        x = nnfunc.max_pool2d(x,2,2)
+        x = x.view(-1,self.linear1_in_features)
+        x = nnfunc.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = nnfunc.softmax(x,dim=1)
+        return x
     def start(self):
         print('1 - show sample')
         print('2 - train')
@@ -25,9 +52,10 @@ class EmnistNN(nn.Module):
         if i=='1':
            self.show_sample()
         elif i=='2':
-           pass
+           imgs = self.dataset.data[0:1]
+           print(self(imgs).detach().numpy().astype(np.float32))
     def load_dataset(self):
-        self.dataset = datasets.EMNIST(root=self.root_dir,split='byclass',download=True)
+        self.dataset = datasets.EMNIST(root=self.root_dir,split='byclass',download=True,transform=transforms.Compose(transforms.ToTensor()))
         train_idxs, test_idxs = train_test_split(list(range(len(self.dataset))), test_size=self.test_sz,shuffle=True)
     
         self.test_ds = Subset(self.dataset,test_idxs)
